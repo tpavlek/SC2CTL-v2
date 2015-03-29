a689adc9e22553e4119cfaa07b6796247bf786ff
Troy Pavlek <troy@depotwarehouse.net>
2015-03-29 05:43:35 -0600

    Added show 4

--- resources/views/jeopardy/index.blade.php
+++ resources/views/jeopardy/index.blade.php
@@ -54,44 +54,7 @@
         @endif
 
         @if (isset($next_show))
-        <div class="pure-g">
-            <div class="pure-u-1 pure-u-md-1-3">
-                <h3>TBD</h3>
-                <div class="contestant" style="display: none;">
-                    <img src="/img/jeopardy/3/contestants/niceusername.jpg">
-                    <a href="https://twitter.com/nice__username">@nice__username</a>
-                    <p>
-                        Joe "nice__username" is on reddit a lot and does a lot of video editing. You've probably seen
-                        his highlights that he contributes to SC2HL.com, or the weekly ReMax recaps that he edits.
-                    </p>
-
-                </div>
-            </div>
-            <div class="pure-u-1 pure-u-md-1-3">
-                <h3>TBD</h3>
-                <div class="contestant" style="display:none;">
-                    <img src="/img/jeopardy/3/contestants/jakatak.jpg">
-                    <a href="https://twitter.com/JaKaTaKtv">@JaKaTaKtv</a>
-                    <p>
-                        Thomas "JaKaTaK" Labonte is a content producer for Starcraft 2 who contributed many videos to the long running
-                        channel Tutorial Central. These days, he produces a lot of content for
-                        <a href="https://www.youtube.com/user/jakataktv">his YouTube channel.</a>
-                    </p>
-
-                </div>
-            </div>
-            <div class="pure-u-1 pure-u-md-1-3">
-                <div class="contestant">
-                    <img src="/img/jeopardy/2/contestants/pengwin.jpg">
-                    <a href="https://twitter.com/myipengwin">@mYiPengWin</a>
-                    <small><i class="fa fa-trophy"></i> Returning Champion</small>
-                    <p>
-                        Stefan "PengWin" Mott is a professional Starcraft 2 Player and Caster. He is currently the
-                        captain of mYinsanity and casts various events include WCS EU Challenger League.
-                    </p>
-                </div>
-            </div>
-        </div>
+            @include('jeopardy.shows.4')
         @endif
 
 
---
+++ resources/views/jeopardy/shows/4.blade.php
@@ -0,0 +1,34 @@
+<div class="pure-g">
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="contestant">
+            <img src="/img/jeopardy/4/contestants/jade.jpg">
+            <a href="https://twitter.com/ESC_Jade">@ESC_Jade</a>
+            <p>
+                Mikolaj "Jade" Andrzejak is a protoss player hailing from Poland. He currently plays for the team ESC Gaming.
+            </p>
+
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="contestant">
+            <img src="/img/jeopardy/4/contestants/ploguidic.jpg">
+            <a href="https://twitter.com/ploguidic3">@ploguidic3</a>
+            <p>
+                Joe "ploguidic3" LoGuidice is the Assistant Editor and Head of StarCraft content for eSports Heaven.
+                He also help with the English streams for lots of Chinese tournaments like Kung Fu Cup, GPL, WCS China, and Leifing.
+            </p>
+
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="contestant">
+            <img src="/img/jeopardy/2/contestants/pengwin.jpg">
+            <a href="https://twitter.com/myipengwin">@mYiPengWin</a>
+            <small><i class="fa fa-trophy"></i> Returning Champion (35,000 points)</small>
+            <p>
+                Stefan "PengWin" Mott is a professional Starcraft 2 Player and Caster. He is currently the
+                captain of mYinsanity and casts various events include WCS EU Challenger League.
+            </p>
+        </div>
+    </div>
+</div>
