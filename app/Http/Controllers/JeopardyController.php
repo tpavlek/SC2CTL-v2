553018e510fa1199d3bf7c1b382b0f3bcf9aa062
Troy Pavlek <troy@depotwarehouse.net>
2015-04-21 21:53:23 -0600

    Episode 7

--- app/Http/Controllers/JeopardyController.php
+++ app/Http/Controllers/JeopardyController.php
@@ -17,7 +17,7 @@ class JeopardyController extends Controller
      */
     public function index()
     {
-        $next_show = new Carbon("April 21, 2015 7PM", new DateTimeZone('America/Edmonton'));
+        $next_show = new Carbon("May 5, 2015 7PM", new DateTimeZone('America/Edmonton'));
 
         // If we've passed the next show date, we don't want to include it in the view.
         if (Carbon::now(new DateTimeZone('America/Edmonton'))->lt($next_show)) {
---
+++ resources/views/jeopardy/shows/7.blade.php
@@ -0,0 +1,35 @@
+<h2><em>Episode 7: "mYidon't know the answer, troy!"</em></h2>
+<div class="pure-g">
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="contestant">
+            <img src="/img/jeopardy/7/contestants/queene.jpg">
+            <a href="https://twitter.com/mYiQueenE">@mYiQueenE</a>
+            <p>
+                Emily "QueenE" Krumlinde is a swedish protoss player playing for My Insanity. She's a fan of Pizza.
+            </p>
+
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="contestant">
+            <img src="/img/jeopardy/7/contestants/arium.jpg">
+            <a href="https://twitter.com/mYiArium">@mYiArium</a>
+            <p>
+                Samson "Arium" Nazaryan is a 15-year old professional protoss player from southern california. When not
+                going to school, he is inelligible to play in WCS because of his age.
+            </p>
+
+        </div>
+    </div>
+    <div class="pure-u-1 pure-u-md-1-3">
+        <div class="contestant">
+            <img src="/img/jeopardy/2/contestants/pengwin.jpg">
+            <a href="https://twitter.com/myipengwin">@mYiPengWin</a>
+            <small><i class="fa fa-trophy"></i> Returning Champion (70,531 points)</small>
+            <p>
+                Stefan "PengWin" Mott is a professional Starcraft 2 Player and Caster. He is currently the
+                captain of mYinsanity and casts various events include WCS EU Challenger League.
+            </p>
+        </div>
+    </div>
+</div>
